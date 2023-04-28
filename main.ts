a8b58aa90d21db2f895e8c4173cde97702c967e6
fishiversity <41026299+fishiversity@users.noreply.github.com>
2023-04-28 08:30:51 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
 radio.onReceivedNumber(function (receivedNumber) {
     activity = receivedNumber
-    basic.showNumber(receivedNumber)
 })
+function forkDown () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
+}
+function stop () {
+    wuKong.stopAllMotor()
+}
+function reverse () {
+    wuKong.setAllMotor(-20, -20)
+}
+function forward () {
+    wuKong.setAllMotor(20, 20)
+}
+function forkUp () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 60)
+}
 let activity = 0
 wuKong.stopAllMotor()
 radio.setGroup(40)
 basic.showIcon(IconNames.Pitchfork)
+basic.forever(function () {
+    if (activity == 1) {
+        forward()
+    } else if (activity == 2) {
+        reverse()
+    } else if (activity == 6) {
+        forkUp()
+    } else if (activity == 7) {
+        forkDown()
+    } else {
+        stop()
+    }
+})
